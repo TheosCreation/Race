1d5c757a2feab3ff14f36c631f42642afecd251c
k4x1MDS <141291975+k4x1MDS@users.noreply.github.com>
2024-03-03 16:21:30 +1300

    collection of warcrimes

--- Race/Assets/ObjFloating.cs
+++ Race/Assets/ObjFloating.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 
 public class ObjFloating : MonoBehaviour
 {
-    private TimeSystem t;
+    public GameObject TimeSystem;
+    public TimeSystem t;
     // Start is called before the first frame update
     void Start()
     {
-        t = GetComponentInParent<TimeSystem>();
+       
+        t = TimeSystem.GetComponent<TimeSystem>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.localPosition = new Vector3(transform.localPosition.x,
-            Mathf.Sin(transform.localPosition.x + t.time+ transform.localPosition.z)/3,
+            (Mathf.Sin(transform.localPosition.x + t.time+ transform.localPosition.z)/3)+transform.parent.transform.position.y,
             transform.localPosition.z);
     }
 }
