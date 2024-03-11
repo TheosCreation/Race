ef7d23613044ed405706d6e0bda81b1a226734e1
k4x1MDS <141291975+k4x1MDS@users.noreply.github.com>
2024-03-11 16:20:53 +1300

    race

--- Race/Assets/Scripts/MeshDestroy.cs
+++ Race/Assets/Scripts/MeshDestroy.cs
@@ -62,8 +62,7 @@ public class MeshDestroy : MonoBehaviour
                 bounds.Expand(0.5f);
 
                 var plane = new Plane(UnityEngine.Random.onUnitSphere, new Vector3(UnityEngine.Random.Range(bounds.min.x, bounds.max.x),
-                                                                                   UnityEngine.Random.Range(bounds.min.y, bounds.max.y),
-                                                                                   UnityEngine.Random.Range(bounds.min.z, bounds.max.z)));
+                                                                                   UnityEngine.Random.Range(bounds.min.y, bounds.max.y)));
 
 
                 subParts.Add(GenerateMesh(parts[i], plane, true));
